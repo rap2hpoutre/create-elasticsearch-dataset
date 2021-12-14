870b9d454344f8ac201b4d4c2ce248b9ea6f8ec7
Raphaël Huchet <raph@selego.co>
2021-12-14 22:32:32 +0100

    fix: use yargs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-export default function add(a: number, b: number): number {
-  return a + b;
-}
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+yargs(hideBin(process.argv))
+  .command(
+    "curl <url>",
+    "fetch the contents of the URL",
+    () => {},
+    (argv) => {
+      console.info(argv);
+    }
+  )
+  .demandCommand(1)
+  .parse();
