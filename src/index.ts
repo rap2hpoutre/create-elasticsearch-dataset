d30713bca46de26ca54d8e80ec9e079c2fb029ba
Raphaël Huchet <raph@selego.co>
2021-12-16 17:59:31 +0100

    feat: it works

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,34 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
+import { importCsv } from "csv-elasticsearch";
+import { Client } from "@elastic/elasticsearch";
 
 yargs(hideBin(process.argv))
+  .scriptName("create-elasticsearch-dataset")
+  .locale("en")
   .command(
-    "curl <url>",
-    "fetch the contents of the URL",
-    () => {},
-    (argv) => {
-      console.info(argv);
+    "$0",
+    "Generate a dataset for elasticsearch",
+    (yargs) => {
+      return yargs.option("name", {
+        alias: "n",
+        describe: "name of the dataset",
+        choices: ["books", "movies"],
+        type: "string",
+      });
+    },
+    async (argv) => {
+      if (argv.name === "books") {
+        const { count } = await importCsv({
+          client: new Client({
+            node: "http://localhost:9200",
+          }),
+          index: "books",
+          filePath: "./data/books.csv",
+        });
+        console.log(`Imported ${count} documents in index 'books'`);
+      }
     }
   )
-  .demandCommand(1)
+  .help()
   .parse();
